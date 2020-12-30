c9612ee37ca880d7f2e90a2a6792258e370876a7
seymaerbas <seymaerbas0619@gmail.com>
2020-12-31 01:44:34 +0300

    Route tanımlandı. Login,Logincheck Fonksiyonu oluşturuldu. login.blade.php sayfası oluşturuldu.

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,12 @@ Route::get('/test/{id}/{name}', [HomeController::class, 'test'])->whereNumber('i
 
 Route::get('/admin', [\App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin_home');
 
+Route::get('/admin/login', [HomeController::class, 'login'])->name('admin_login');
+
+Route::post('/admin/logincheck', [HomeController::class, 'logincheck'])->name('admin_logincheck');
+
+Route::get('/admin/logout', [HomeController::class, 'logout'])->name('admin_logout');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');

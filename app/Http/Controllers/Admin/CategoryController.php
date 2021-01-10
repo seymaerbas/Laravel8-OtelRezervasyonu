586d077d799870af24b2e97a0e4054d443f2c723
seymaerbas <seymaerbas0619@gmail.com>
2021-01-10 03:21:29 +0300

    Kategori tablosu oluşturuldu.

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
@@ -15,8 +16,10 @@ class CategoryController extends Controller
      */
     public function index()
     {
-
-       return view('admin.category');
+        $datalist = DB::select('select * from categories');
+        //print_r($datalist);
+        //exit();
+        return view('admin.category',['datalist' => $datalist]);
     }
 
     /**
--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -24,13 +24,49 @@
 
                 <h3 class="card-title">Category List</h3>
             </div>
+            <!-- DataTable with Hover -->
+            <div class="col-lg-12">
+                <div class="card mb-4">
 
-            <div class="card-body">
+                    <div class="table-responsive p-3">
+                        <table class="table align-items-center table-flush table-hover" id="dataTableHover">
+                            <thead class="thead-light">
+                            <tr>
+                                <th>Id</th>
+                                <th>Parent</th>
+                                <th>Title</th>
+                                <th>Status</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                            </thead>
+                            @foreach($datalist as $rs )
 
-                Body Area
 
-            </div>
-
+                            <tbody>
+                            <tr>
+                                <td>{{$rs->id}}</td>
+                                <td>{{$rs->parent_id}}</td>
+                                <td>{{$rs->title}}</td>
+                                <td>{{$rs->status}}</td>
+                                <td>Edit</td>
+                                <td>Delete</td>
+                            </tr>
+                            </tbody>
+                            @endforeach
+                        </table>
+                    </div>
+                    <!-- Modal Logout -->
+                    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelLogout"
+                         aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabelLogout">Ohh No!</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
             <div class="card-footer">
                 Footer
             </div><!-- /footer-->
@@ -41,5 +77,25 @@
 
 </div>
 
+
+@endsection
+@section('footer')
+
+    <script src="{{asset('assets')}}/admin/vendor/jquery/jquery.min.js"></script>
+    <script src="{{asset('assets')}}/admin/vendor/jquery/jquery.min.js"></script>
+    <script src="{{asset('assets')}}/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('assets')}}/admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="{{asset('assets')}}/admin/js/ruang-admin.min.js"></script>
+    <!-- Page level plugins -->
+    <script src="{{asset('assets')}}/admin/vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="{{asset('assets')}}/admin/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script>
+        $(document).ready(function () {
+            $('#dataTable').DataTable(); // ID From dataTable
+            $('#dataTableHover').DataTable(); // ID From dataTable with Hover
+        });
+    </script>
 
 @endsection
